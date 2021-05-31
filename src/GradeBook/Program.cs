572aff0b5ccc3e35cef35257bb7efcc286142879
briancobbel <bcobbel@gmail.com>
2021-05-31 10:21:34 -0500

    initial commit

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var book = new Book("Brian's Grade Book");
+
+            var done = false;
+
+            while(!done)
+            {
+                Console.WriteLine("Enter a grade or 'q' to quit");
+                var input = Console.ReadLine();
+                
+                if(input == "q") {
+                    break;
+                }
+
+                try
+                {
+                    var grade = double.Parse(input);
+                    book.AddGrade(grade);
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+
+
+            }
+
+            var stats = book.GetStats();
+
+            Console.WriteLine($"The lowest grade is {stats.Low}.");
+            Console.WriteLine($"The highest grade is {stats.High}.");
+            Console.WriteLine($"The average grade is {stats.Average:N1}.");
+            Console.WriteLine($"The letter grade is {stats.Letter}.");
+        }
+    }
+
+}
